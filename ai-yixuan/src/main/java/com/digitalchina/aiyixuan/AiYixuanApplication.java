c3064dd281b1ebd73dfe59abcc5cbea0de3a0b88
ACEN1102 <107985232+ACEN1102@users.noreply.github.com>
2023-09-15 15:24:37 +0800

    Add files via upload

---
+++ ai-yixuan/src/main/java/com/digitalchina/aiyixuan/AiYixuanApplication.java
@@ -0,0 +1,13 @@
+package com.digitalchina.aiyixuan;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AiYixuanApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AiYixuanApplication.class, args);
+    }
+
+}
